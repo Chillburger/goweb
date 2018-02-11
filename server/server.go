daa9a7bb9229e3e2a8cc9e5584d4c2016ba58aaa
Chillburger <letitbespliced7@gmail.com>
2018-02-11 14:50:32 -0500

    initial commit

---
+++ server/server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+  "fmt"
+  "io/ioutil"
+)
+
+type Page struct {
+  Title strings
+  Body []byte
+}
+
+func (p *Page) save() error {
+  filename := p.Title + ".txt"
+  return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+
+func main() {
+
+}
